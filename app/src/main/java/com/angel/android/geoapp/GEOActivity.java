f9af64004d00ae45ac41d8ceae4c49c6659fbbc1
AngelJavierPoolMoo <14470254@itcampeche.edu.mx>
2016-05-19 09:46:13 -0500

    Version con demostracion de manejo de eventos del proyecto Geoapp

--- app/src/main/java/com/angel/android/geoapp/GEOActivity.java
+++ app/src/main/java/com/angel/android/geoapp/GEOActivity.java
@@ -2,12 +2,38 @@ package com.angel.android.geoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GEOActivity extends AppCompatActivity {
+
+    private Button mBotonCierto;
+    private Button mBotonFalso;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
+
+        mBotonCierto = (Button) findViewById(R.id.boton_cierto);
+        mBotonFalso = (Button) findViewById(R.id.boton_falso);
+
+        mBotonCierto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GEOActivity.this, R.string.texto_correcto, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        mBotonFalso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GEOActivity.this, R.string.texto_incorrecto, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
